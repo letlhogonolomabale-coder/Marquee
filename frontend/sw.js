4f558fb5d2050ce94a02e09f4802b7b384c07023
letlhogonolomabale-coder <letlhogonolomabalee@gmail.com>
2026-09-14 03:15:11 +0200

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -6,7 +6,7 @@
 //
 // Bump CACHE_NAME whenever you change Marquee.html so old clients pick up
 // the new version instead of serving a cached copy forever.
-const CACHE_NAME = 'marquee-shell-v2';
+const CACHE_NAME = 'marquee-shell-v3';
 const SHELL_FILES = ['/', '/Marquee.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
